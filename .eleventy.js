814165230a95e4d332cd7e1cf4b1cb2d3fe27862
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2025-06-03 17:18:10 +0100

    switch to load base from CDN
    Netlify wont load from node_modules. Yet, 11ty must include node_modules in path prior to addPassthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,5 @@
 
 module.exports = function (config) {
-  config.addPassthroughCopy("**/node_modules/@toheeb/base/base.css");
-  config.addPassthroughCopy("**/node_modules/@toheeb/base/base-settings.js");
   config.addPassthroughCopy("**/*.woff");
   config.addPassthroughCopy("**/*.woff2");
 };
